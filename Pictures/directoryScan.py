06284ed9b96eece768510efe4f0e35105a13a39b
Mehmet <m.kacal95@gmail.com>
2025-06-30 12:26:03 +0200

    latest

--- Pictures/directoryScan.py
+++ Pictures/directoryScan.py
@@ -1,6 +1,6 @@
 import os
 
-def list_files_recursive(input_folder, output_folder, output_filename="file_list.txt"):
+def list_files_recursive(input_folder, output_folder, output_filename="file_list1.txt"):
     output_path = os.path.join(output_folder, output_filename)
     with open(output_path, 'w') as f:
         for root, _, files in os.walk(input_folder):
@@ -10,7 +10,9 @@ def list_files_recursive(input_folder, output_folder, output_filename="file_list
     print(f"File list saved to: {output_path}")
 
 # Example usage:
-input_dir = '/Users/mehmet/workspaces/VerandaFormTry/assets/imgs/'
+# input_dir = '/Users/mehmet/workspaces/VerandaFormTry/assets/imgs/'
+# output_dir = '/Users/mehmet/Business/Pictures/ProductChoice Images/'
+input_dir = '/Users/mehmet/Business/Pictures/ProductChoice Images/demo1Full-Adjusted/'
 output_dir = '/Users/mehmet/Business/Pictures/ProductChoice Images/'
 list_files_recursive(input_dir, output_dir)
 
